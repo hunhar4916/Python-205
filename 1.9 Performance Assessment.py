770e3eb5c516f3bc3aca042487f28f9555a6b3d6
Hunter Harwood <hunhar4916@students.ecpi.edu>
2026-07-29 09:24:06 -0400

    Add files via upload

---
+++ 1.9 Performance Assessment.py
@@ -0,0 +1,18 @@
+print("Hunter Harwood - hunhar4916")
+
+name = input("What is your name: ")
+studentID = input("What is your studentID: ")
+
+num1 = int(input("Please enter a whole number: "))
+num2 = int(input("Please enter another whole number: "))
+
+add = num1 + num2
+sub = num1 - num2
+mult = num1 * num2
+
+print(f"{num1} plus {num2} equals: {add:.2f}")
+
+print(f"{num1} minus {num2} equals: {sub:.2f}")
+
+print(f"{num1} times {num2} equals: {mult:.2f}")
+           
